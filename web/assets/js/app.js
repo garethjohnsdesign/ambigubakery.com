9f7149b5dfdcfaf1e3d7f83c7db5a3b375d440d5
Gareth Johns <info@garethjohnsdesign.com>
2019-12-21 17:53:49 +0000

    Initial commit

---
+++ web/assets/js/app.js
@@ -0,0 +1,106 @@
+// 1. Imports
+// ----------
+
+import $ from "jquery";
+import Foundation from 'foundation-sites';
+import "lightGallery";
+import "lg-fullscreen";
+import "lg-video";
+import "lg-autoplay";
+import AOS from 'aos';
+import Swiper from 'swiper';
+import Cookies from 'js-cookie'
+
+
+// 2. Foundation
+// ----------
+  
+Foundation.Interchange.SPECIAL_QUERIES['medium-retina'] = 'only screen and (min-width: 40em), (min-width: 40em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 40em) and (min--moz-device-pixel-ratio: 2), (min-width: 40em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 40em) and (min-device-pixel-ratio: 2), (min-width: 40em) and (min-resolution: 192dpi), (min-width: 40em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['large-retina'] = 'only screen and (min-width: 64em), (min-width: 64em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 64em) and (min--moz-device-pixel-ratio: 2), (min-width: 64em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 64em) and (min-device-pixel-ratio: 2), (min-width: 64em) and (min-resolution: 192dpi), (min-width: 64em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['xlarge-retina'] = 'only screen and (min-width: 75em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min-width: 90em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
+  
+$(document).foundation();
+
+
+// 1. Loader
+// ---------------
+
+if ($("body").hasClass("home")) {
+  $( ".loader" ).removeClass( "hide" );
+  $( ".loader" ).addClass( "loading" );
+  setTimeout( function() {
+    $( ".loader" ).addClass( "loaded" );
+  }, 2500 
+  );
+}
+
+// 4. Animate on Scroll
+// --------------------
+
+$(function() {
+  AOS.init({ 
+   offset: 64,
+   easing: 'ease-in-out-quart', 
+   duration: 600
+   });   
+});
+
+$(function() {
+window.addEventListener('load', AOS.refresh);
+});
+
+
+// 5. Carousel
+// -----------
+
+$(document).ready(function () {
+
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+
+      direction: 'horizontal',
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      loop: true,
+      preventClicks: true,
+      slideToClickedSlide: false,
+
+  autoplay: {
+    delay: 5000,
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+    navigation: {
+      nextEl: '.swiper-button-next-test',
+      prevEl: '.swiper-button-prev-test',
+    }
+
+   
+    })
+  });
+
+// 6. Lightgallery
+// ---------------
+
+$('.video').lightGallery({
+    counter: false,
+    videoMaxWidth: '1080px',
+    youtubePlayerParams: {
+        modestbranding: 1,
+        showinfo: 0,
+        rel: 0,
+        autoplay: 0,
+    },
+    vimeoPlayerParams: {
+        autoplay: 0,
+        title : 0,
+        byline : 0,
+        portrait : 0,
+        color : 'FFFFFF'     
+    }
+});
